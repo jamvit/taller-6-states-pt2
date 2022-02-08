45681505aabe689b5c66f6c3439e8aed7e0ea9c3
jamvit <ot12341@gmail.com>
2022-02-07 20:07:00 -0600

    init

---
+++ src/components/TableBody.js
@@ -0,0 +1,24 @@
+const TableBody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>janitor</td>
+      </tr>
+      <tr>
+        <td>Julie</td>
+        <td>mechanic</td>
+      </tr>
+      <tr>
+        <td>Juan</td>
+        <td>actor</td>
+      </tr>
+      <tr>
+        <td>Pedro</td>
+        <td>admin</td>
+      </tr>
+    </tbody>
+  );
+};
+
+export default TableBody;
